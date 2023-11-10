3eaaf012200f7349db75e1361380cf60a169e84b
aruiz-mo <aruiz-mo@iMac-de-aruiz-mo.local>
2023-11-10 15:40:27 +0100

    Cambios logs y comprobar nick

--- ft_irc/ComunicationUtils.cpp
+++ ft_irc/ComunicationUtils.cpp
@@ -6,13 +6,13 @@
 /*   By: aruiz-mo <aruiz-mo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 10:52:58 by aruiz-mo          #+#    #+#             */
-/*   Updated: 2023/11/08 15:03:54 by aruiz-mo         ###   ########.fr       */
+/*   Updated: 2023/11/10 13:53:43 by aruiz-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "IrcServ.hpp"
 
-void log(const std::string& message)
+void logMessage(const std::string& message)
 {
 	time_t      rawtime;
     struct tm   *timeinfo;
@@ -25,4 +25,11 @@ void log(const std::string& message)
     std::string str(buffer);
 	std::cout << "\033[0;34m[" << str << "]\033[0m ";
     std::cout << message << std::endl;
+}
+
+void    clientMessage(const std::string& message, int fd)
+{
+    std::string buffer = message + "\r\n";
+    if (send(fd, buffer.c_str(), buffer.length(), 0) < 0)
+        std::cerr << "Error send message." << std::endl;
 }
